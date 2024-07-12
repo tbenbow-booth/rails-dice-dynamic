c4aac6a61193857d8175099fa959d96342145a3d
tbenbow-booth <tbenbow@chicagobooth.edu>
2024-07-12 01:58:37 +0000

    74% Complete

---
+++ app/controllers/casino_controller.rb
@@ -0,0 +1,57 @@
+class CasinoController < ApplicationController
+  def slots
+    render ({ :template => "monaco_templates/home"})
+  end
+
+
+  def cards
+    @rolls = []    # Create a blank array
+
+  2.times do    # 2 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+  
+    render ({ :template => "monaco_templates/two_six"})
+  end
+
+
+  def bet
+    @rolls = []    # Create a blank array
+
+    2.times do    # 2 times...
+      die = rand(1..10)    # Generate a random number
+  
+      @rolls.push(die)    # Add the random number to the array
+    end
+
+    render ({ :template => "monaco_templates/two_ten"})
+  end
+
+
+  def jackpot
+    @rolls = []    # Create a blank array
+
+    1.times do    # 1 times...
+      die = rand(1..20)    # Generate a random number
+  
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render ({ :template => "monaco_templates/one_twenty"})
+  end
+
+
+  def blackjack
+    @rolls = []    # Create a blank array
+
+    5.times do    # 5 times...
+      die = rand(1..4)    # Generate a random number
+  
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render ({ :template => "monaco_templates/five_four"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+ 
+  get ("/") , { :controller => "casino" , :action => "slots"}
 
+  get ("/dice/2/6") , { :controller => "casino" , :action => "cards"}
+
+  get ("/dice/2/10") , { :controller => "casino" , :action => "bet"}
+
+  get ("/dice/1/20") , { :controller => "casino" , :action => "jackpot"}
+
+  get ("/dice/5/4") , { :controller => "casino" , :action => "blackjack"}
 end
